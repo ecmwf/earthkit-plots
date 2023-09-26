c54a1cb9b0e0a7bf80a39d3e35043bef436342ab
James Varndell <james.varndell@ecmwf.int>
2023-09-26 11:52:45 +0100

    Adds experimental globe plotting feature

--- earthkit/plots/globe.py
+++ earthkit/plots/globe.py
@@ -14,98 +14,151 @@
 
 import earthkit.maps
 
+import warnings
+import io
+import matplotlib
+from matplotlib.gridspec import GridSpec
 import plotly.graph_objects as go
 import numpy as np
 from numpy import pi, sin, cos
+from PIL import Image
 
 
-def globe(data, *args, **kwargs):
-    latlon = data.to_latlon()
-    lat = latlon["lat"]
-    lon = latlon["lon"]
-    values = data.to_numpy()
+warnings.warn("earthkit.plots.globe is an EXPERIMENTAL feature")
+
+
+RADIUS = 1
+
+
+def style_to_grayscale(style):
+    colors = [(0, 0, 0)]
     
-    # # Shift 'lon' from [0,360] to [-180,180]
-    # tmp_lon = np.array(
-    #     [lon[n]-360 if l>=180 else lon[n] for n,l in enumerate(lon)])
+    if style.gradients:
+        gradient_range = sum(style.gradients)
+        for gradient in style.gradients:
+            increment = colors[-1][0] + gradient/gradient_range
+            colors.append((increment, increment, increment))
+    else:
+        level_range = abs(style._levels[-1]-style._levels[0])
+        for i in range(len(style._levels)-1):
+            increment = (style._levels[i+1]-style._levels[i])/level_range
+            colors.append[(increment, increment, increment)]
+
+    new_style = style.__class__(
+        colors=colors,
+        levels=style._levels,
+        gradients=style.gradients,
+        units=style._units,
+        **style.kwargs,
+    )
     
-    # i_east, = np.where(tmp_lon>=0)  # indices of east lon
-    # i_west, = np.where(tmp_lon<0)   # indices of west lon
-    # lon = np.hstack((tmp_lon[i_west], tmp_lon[i_east]))  # stack the 2 halves
+    return new_style
 
-    # # Correspondingly, shift the values array
-    # values_ground = np.array(values)
-    # values = np.hstack((values_ground[:,i_west], values_ground[:,i_east]))
+
+def matplotlib_to_plotly(cmap, pl_entries=255):
+    h = 1.0/(pl_entries-1)
+    pl_colorscale = []
+
+    for k in range(pl_entries):
+        C = [np.uint8(item) for item in np.array(cmap(k*h)[:3])*255]
+        pl_colorscale.append([k*h, 'rgb'+str((C[0], C[1], C[2]))])
+
+    return pl_colorscale
+
+
+def globe(data, *args, style, **kwargs):
+    backend = matplotlib.rcParams['backend']
+    matplotlib.use("Agg")
     
-    colorscale=[
-        [0.0, '#313695'],
-        [0.07692307692307693, '#3a67af'],
-        [0.15384615384615385, '#5994c5'],
-        [0.23076923076923078, '#84bbd8'],
-        [0.3076923076923077, '#afdbea'],
-        [0.38461538461538464, '#d8eff5'],
-        [0.46153846153846156, '#d6ffe1'],
-        [0.5384615384615384, '#fef4ac'],
-        [0.6153846153846154, '#fed987'],
-        [0.6923076923076923, '#fdb264'],
-        [0.7692307692307693, '#f78249'],
-        [0.8461538461538461, '#e75435'],
-        [0.9230769230769231, '#cc2727'],
-        [1.0, '#a50026'],
-    ]
-
-    clons=np.array(lon, dtype=np.float64)
-    clats=np.array(lat, dtype=np.float64)
-    clons, clats=np.meshgrid(clons, clats)
+    cmap = style.to_kwargs(data)["cmap"]
+    colorscale = matplotlib_to_plotly(cmap)
     
-    XS, YS, ZS=mapping_map_to_sphere(clons, clats)
+    style = style_to_grayscale(style)
+    
+    gs = GridSpec(1, 1, left=0, right=1, bottom=0, top=1)
+    earthkit.maps.schema.figsize = (6, 3)
+    chart = earthkit.maps.Superplot.from_gridspec(gs)
+    chart.add_subplot(frameon=False)
+    chart.plot(data, style=style)
+    
 
-    nrows, ncolumns=clons.shape
-    OLR=np.zeros(clons.shape, dtype=np.float64)
-    OLR[:, :ncolumns-1]=np.copy(np.array(values,  dtype=np.float64))
-    OLR[:, ncolumns-1]=np.copy(values[:, 0])
+    buf = io.BytesIO()
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        chart.save(buf, format="png")
+    matplotlib.use(backend)
+    buf.seek(0)
+    
+    texture = np.asarray(Image.open(buf)).T[0]
+    shape = texture.shape
+    
+    texture = np.array(normalize(texture, (0, 255), (style._levels[0], style._levels[-1]))).reshape(shape)
+    
+    lons = np.linspace(-180, 180-(360/shape[0]), shape[0])
+    lats = np.linspace(-90, 90-(180/shape[1]), shape[1])
+    
+    lons, lats = np.meshgrid(lons, lats)
 
+    # text=[['lon: '+'{:.2f}'.format(lons[i,j])+'<br>lat: '+'{:.2f}'.format(lats[i, j])
+    #      for j in range(shape[0])] for i in range(shape[1])]
 
-    sphere=dict(type='surface',
-            x=XS, 
-            y=YS, 
-            z=ZS,
-            colorscale=colorscale,
-            surfacecolor=OLR,
-            cmin=-20, 
-            cmax=20,
-            colorbar=dict(thickness=20, len=0.75, ticklen=4, title= 'W/m²'),
+    x,y,z = sphere(RADIUS ,texture, lats, lons)
+    # x, y, z=mapping_map_to_sphere(x, y)
+    surf = go.Surface(x=x, y=y, z=z,
+                    surfacecolor=texture,
+                    colorscale=colorscale,
+                    # text=text,
+                    # hoverinfo='text'
+                    )    
+
+    layout = go.Layout(
+        scene=dict(
+            aspectratio=dict(x=1, y=1, z=1),
+            xaxis = dict(visible=False),
+            yaxis = dict(visible=False),
+            zaxis =dict(visible=False),
+            camera=dict(eye=dict(x=1.15, 
+                                        y=1.15, 
+                                        z=1.15)
+                                        )
         )
+    )
 
-    noaxis=dict(showbackground=False,
-            showgrid=False,
-            showline=False,
-            showticklabels=False,
-            ticks='',
-            title='',
-            zeroline=False)
+    fig = go.Figure(data=[surf], layout=layout)
+    buf.close()
 
-    layout3d=dict(title='Outgoing Longwave Radiation Anomalies<br>Dec 2017-Jan 2018',
-              font=dict(family='Balto', size=14),
-              width=800, 
-              height=800,
-              scene=dict(xaxis=noaxis, 
-                         yaxis=noaxis, 
-                         zaxis=noaxis,
-                         aspectratio=dict(x=1,
-                                          y=1,
-                                          z=1),
-                         camera=dict(eye=dict(x=1.15, 
-                                     y=1.15, 
-                                     z=1.15)
-                                    )
-            ),
-           )
-
-    fig=go.Figure(data=[sphere], layout=layout3d)
     return fig
 
 
+def sphere(size, texture, lats=None, lons=None): 
+    N_lat = int(texture.shape[0])
+    N_lon = int(texture.shape[1])
+    theta = np.linspace(0,2*np.pi,N_lat)
+    phi = np.linspace(0,np.pi,N_lon)
+    
+    # Set up coordinates for points on the sphere
+    x0 = size * np.outer(np.cos(theta),np.sin(phi))
+    y0 = size * np.outer(np.sin(theta),np.sin(phi))
+    z0 = size * np.outer(np.ones(N_lat),np.cos(phi))
+    
+    # if lats is not None:
+    #     lon=np.array(lons, dtype=np.float64)
+    #     lat=np.array(lats, dtype=np.float64)
+    #     lon=degree2radians(lon)
+    #     lat=degree2radians(lat)
+    #     x0=RADIUS*cos(lon)*cos(lat)
+    #     y0=RADIUS*sin(lon)*cos(lat)
+    #     z0=RADIUS*sin(lat)
+        
+    
+    # Set up trace
+    return x0,y0,z0
+
+
+def normalize(values, actual_bounds, desired_bounds):
+    return [
+        desired_bounds[0] + (x - actual_bounds[0]) * (desired_bounds[1] - desired_bounds[0]) / (actual_bounds[1] - actual_bounds[0]) for y in values for x in y
+    ]
 
 
 def degree2radians(degree):
@@ -113,7 +166,7 @@ def degree2radians(degree):
     return degree*pi/180
 
 
-def mapping_map_to_sphere(lon, lat, radius=1):
+def mapping_map_to_sphere(lon, lat, radius=RADIUS):
     #this function maps the points of coords (lon, lat) to points onto the  sphere of radius radius
     
     lon=np.array(lon, dtype=np.float64)
@@ -124,32 +177,3 @@ def mapping_map_to_sphere(lon, lat, radius=1):
     ys=radius*sin(lon)*cos(lat)
     zs=radius*sin(lat)
     return xs, ys, zs
-
-
-def polygons_to_traces(poly_paths, N_poly):
-    ''' 
-    pos arg 1. (poly_paths): paths to polygons
-    pos arg 2. (N_poly): number of polygon to convert
-    '''
-    # init. plotting list
-    lons=[]
-    lats=[]
-
-    for i_poly in range(N_poly):
-        poly_path = poly_paths[i_poly]
-        
-        # get the Basemap coordinates of each segment
-        coords_cc = np.array(
-            [(vertex[0],vertex[1]) 
-             for (vertex,code) in poly_path.iter_segments(simplify=False)]
-        )
-        
-        # convert coordinates to lon/lat by 'inverting' the Basemap projection
-        lon_cc, lat_cc = m(coords_cc[:,0],coords_cc[:,1], inverse=True)
-    
-        
-        lats.extend(lat_cc.tolist()+[None]) 
-        lons.extend(lon_cc.tolist()+[None])
-        
-       
-    return lons, lats
