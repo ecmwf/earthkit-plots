7476be7c782f2f4fe964ef5f2b1533bfe24705fc
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2025-04-02 10:04:38 +0100

    QA and missing imports

--- src/earthkit/plots/geo/grids.py
+++ src/earthkit/plots/geo/grids.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import importlib.util
+import re
 import warnings
 
 # import cartopy.crs as ccrs
