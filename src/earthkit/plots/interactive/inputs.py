111681f0479920178abb4713dd19d2c190731dce
nightowl2626 <178183948+nightowl2626@users.noreply.github.com>
2025-09-15 10:48:07 +0200

    Feature/heatmap (#90)
    * feat: Interactive heatmaps
    
    * Clean up charts.py
    
    * Clean up charts.py
    
    * Clean up inputs.py
    
    * Clean up heat function and notebook
    
    * Delete accidental file
    
    * Delete accidental file (cache)
    
    * Added markdown comments to example notebook
    
    * Added markdown comments to example notebook
    
    ---------
    
    Co-authored-by: James Varndell <36902610+JamesVarndell@users.noreply.github.com>

--- src/earthkit/plots/interactive/inputs.py
+++ src/earthkit/plots/interactive/inputs.py
@@ -16,6 +16,7 @@ import warnings
 
 import earthkit.data
 import numpy as np
+import xarray as xr
 
 from earthkit.plots.interactive import times
 
@@ -43,7 +44,6 @@ def to_pandas(data):
 
 def to_numpy(data):
     return _earthkitify(data).to_numpy()
-
 
 def sanitise(axes=("x", "y"), multiplot=True):
     def decorator(function):
@@ -118,7 +118,6 @@ def sanitise(axes=("x", "y"), multiplot=True):
         return wrapper
 
     return decorator
-
 
 def get_xarray_kwargs(data, axes, kwargs):
     data = to_xarray(data)
