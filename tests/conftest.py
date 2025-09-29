b291f559d8612e50674acf6f24493776f0b550fa
James Varndell <james.varndell@ecmwf.int>
2025-09-29 16:48:51 +0100

    Add copyright message to tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,22 @@
+# Copyright 2024-, European Centre for Medium Range Weather Forecasts.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import shutil
 import subprocess
 import tempfile
 
 import pytest
-
-# Import earthkit.plots to ensure fonts and schema are properly initialized
-
-# Also ensure default styles are loaded
-try:
-    import earthkit_plots_default_styles  # noqa: F401
-except ImportError:
-    pass  # Default styles package not available
 
 
 def pytest_addoption(parser):
