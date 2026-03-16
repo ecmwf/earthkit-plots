c86a26d1a4e0ffebeb85962b52cce6cc71384c61
James Varndell <36902610+JamesVarndell@users.noreply.github.com>
2026-03-16 14:13:36 +0000

    feat: use new ek theme for docs (#175)
    Co-authored-by: oisin-m <oisinmmm@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,22 +1,34 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Import and path setup ---------------------------------------------------
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import datetime
 import os
 import sys
 
+on_rtd = os.environ.get("READTHEDOCS") == "True"
 sys.path.insert(0, os.path.abspath("../"))
 sys.path.insert(0, os.path.abspath("."))
 
 import generate_styles_page  # noqa: E402
 
-from earthkit.plots.version import __version__  # noqa: E402
+if on_rtd:
+    version = os.environ.get("READTHEDOCS_VERSION", "latest")
+    release = version
+else:
+    version = "dev"
+    release = "dev"
 
+rtd_version = version if version != "latest" else "develop"
+rtd_version_type = os.environ.get("READTHEDOCS_VERSION_TYPE", "branch")
+
+if rtd_version_type in ("branch", "tag"):
+    source_branch = rtd_version
+else:
+    source_branch = "main"
 # -- Styles gallery generation -----------------------------------------------
 
 
@@ -25,57 +37,49 @@ generate_styles_page.generate(docs_dir=_docs_dir)
 
 # -- Project information -----------------------------------------------------
 
+sys.path.insert(0, os.path.abspath("../../src"))
 
 project = "earthkit-plots"
-copyright = f"2022-{datetime.datetime.now().strftime('%Y')}, European Centre for Medium Range Weather Forecasts"
-author = "European Centre for Medium Range Weather Forecasts"
-version = __version__
-release = __version__
+copyright = "2025, European Centre for Medium-Range Weather Forecasts (ECMWF)"
+author = "European Centre for Medium-Range Weather Forecasts (ECMWF)"
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
-    "sphinx_rtd_theme",
-    "nbsphinx",
+    # Automatically extracts documentation from your Python docstrings
     "sphinx.ext.autodoc",
+    # Supports Google-style and NumPy-style docstrings
     "sphinx.ext.napoleon",
+    # Renders LaTeX math in HTML using MathJax
+    "sphinx.ext.mathjax",
+    # Option to click viewcode
+    "sphinx.ext.viewcode",
+    # Links to the documentation of other projects via cross-references
     "sphinx.ext.intersphinx",
+    # Generates summary tables for modules/classes/functions
+    # "sphinx.ext.autosummary",
+    # Allows citing BibTeX bibliographic entries in reStructuredText
+    "sphinxcontrib.bibtex",
+    # Tests snippets in documentation by running embedded Python examples
+    # "sphinx.ext.doctest",
+    # Checks documentation coverage of the codebase
+    # "sphinx.ext.coverage",
+    # Adds .nojekyll file and helps configure docs for GitHub Pages hosting
+    # "sphinx.ext.githubpages",
+    # Adds "Edit on GitHub" links to documentation pages
+    # "edit_on_github",
+    # Adds "Edit on GitHub" links to documentation pages
+    # "sphinx_github_style",
+    # Option to link to code
+    # "sphinx.ext.linkcode",
+    # Automatically includes type hints from function signatures into the documentation
+    # "sphinx_autodoc_typehints",
+    # Integrates Jupyter Notebooks into Sphinx
+    "nbsphinx",
+    # Simplifies linking to external resources with short aliases
     "sphinx.ext.extlinks",
-    "autoapi.extension",
 ]
-
-# autodoc configuration
-autodoc_typehints = "none"
-
-# autoapi configuration
-autoapi_dirs = ["../src/earthkit/"]
-autoapi_ignore = [
-    "*/version.py",
-    "sphinxext/*",
-    "*/data/*",
-    "*/healpix.py",
-    "*/reduced_gg.py",
-]
-autoapi_options = [
-    "members",
-    "undoc-members",
-    "show-inheritance",
-    "show-module-summary",
-    "imported-members",
-    "inherited-members",
-]
-autoapi_root = "_api"
-autoapi_member_order = "alphabetical"
-autoapi_add_toctree_entry = False
-
-# GitHub links configuration
-extlinks = {
-    "pr": ("https://github.com/ecmwf/earthkit-plots/pull/%s", "PR #%s"),
-    "issue": ("https://github.com/ecmwf/earthkit-plots/issues/%s", "Issue #%s"),
-}
 
 # GitHub links configuration
 extlinks = {
@@ -86,11 +90,6 @@ extlinks = {
 nbsphinx_thumbnails = {
     "examples/examples/string-formatting-units": "_static/string-formatting-units.png",
 }
-
-# napoleon configuration
-# napoleon_google_docstring = False
-# napoleon_numpy_docstring = True
-# napoleon_preprocess_types = True
 
 # intersphinx configuration
 intersphinx_mapping = {
@@ -115,14 +114,57 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "sphinx_rtd_theme"
-html_logo = "https://raw.githubusercontent.com/ecmwf/logos/refs/heads/main/logos/earthkit/earthkit-plots-dark.svg"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+html_theme = "furo"
+
 html_static_path = ["_static"]
+
+html_css_files = [
+    "custom.css",
+]
+
+bibtex_bibfiles = ["references.bib"]
+
+html_theme_options = {
+    "light_css_variables": {
+        "color-sidebar-background": "#131320",
+        # "color-background-primary": "", # leave as default to avoid overriding the light theme background
+        "color-sidebar-link-text": "#ffffff",
+        "color-sidebar-brand-text": "#ffffff",
+        "color-sidebar-caption-text": "#ffffff",
+        "color-brand-primary": "#FCE54B",
+        "color-brand-content": "#5f8dd3",
+        "color-sidebar-item-background--hover": "#001F3F",
+        "color-sidebar-item-expander-background--hover": "#001F3F",
+    },
+    "dark_css_variables": {
+        "color-sidebar-background": "#131320",
+        "color-background-primary": "#131320",
+        "color-sidebar-link-text": "#ffffff",
+        "color-sidebar-brand-text": "#ffffff",
+        "color-sidebar-caption-text": "#ffffff",
+        "color-brand-primary": "#FCE54B",
+        "color-brand-content": "#5f8dd3",
+        "color-sidebar-item-background--hover": "#001F3F",
+        "color-sidebar-item-expander-background--hover": "#001F3F",
+    },
+    "light_logo": "earthkit-plots-dark.svg",
+    "dark_logo": "earthkit-plots-dark.svg",
+    "source_repository": "https://github.com/ecmwf/earthkit-plots/",
+    "source_branch": source_branch,
+    "source_directory": "docs/source",
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/ecmwf/earthkit-plots",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+    ],
+}
