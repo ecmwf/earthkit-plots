61bd27b51db36af05d9b1fffb9c48843ecafda3e
Harrison <harrison.cook@ecmwf.int>
2026-06-09 10:30:33 +0200

    provide type guard on s missing

--- src/earthkit/plots/styles/__init__.py
+++ src/earthkit/plots/styles/__init__.py
@@ -820,12 +820,12 @@ class Style:
             nan_mask = np.isnan(values.ravel())
             missing_x = x.ravel()[nan_mask]
             missing_y = y.ravel()[nan_mask]
-            missing_s = s.ravel()[nan_mask]
+            missing_s = s.ravel()[nan_mask] if not np.isscalar(s) else s
 
             x = x.ravel()[~nan_mask]
             y = y.ravel()[~nan_mask]
             values = values.ravel()[~nan_mask]
-            s = s.ravel()[~nan_mask]
+            s = s.ravel()[~nan_mask] if not np.isscalar(s) else s
 
             if missing_values:
                 ax.scatter(
