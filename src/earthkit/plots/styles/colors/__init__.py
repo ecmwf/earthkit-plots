0678ac73302ece7fccdcc3560a1083b92eefdc0c
Olivier Burggraaff <olivierburggraaff@gmail.com>
2026-07-28 11:26:46 +0100

    Handle ListedColormaps in expand and cmap_and_norm

--- src/earthkit/plots/styles/colors/__init__.py
+++ src/earthkit/plots/styles/colors/__init__.py
@@ -69,6 +69,8 @@ def expand(colors, levels, extend_colors=0):
             colors = [colors] * (length - 1)
         else:
             colors = [cmap(i) for i in np.linspace(0, 1, length)]
+    elif isinstance(colors, ListedColormap):
+        colors = list(colors.colors)
     elif isinstance(colors, mpl.colors.Colormap):
         colors = [colors(i) for i in np.linspace(0, 1, length)]
     return colors
@@ -108,6 +110,7 @@ def cmap_and_norm(colors, levels, normalize=True, extend=None, extend_levels=Tru
         Whether to extend the levels. If False, the levels will be used as is.
         If True, the levels will be extended to include the under and over values.
     """
+    is_listed = isinstance(colors, ListedColormap)
     levels = list(levels)
     extend_colors = 0
     color_levels = levels
@@ -134,7 +137,6 @@ def cmap_and_norm(colors, levels, normalize=True, extend=None, extend_levels=Tru
     N = len(color_levels) + extend_colors - 1
 
     colormap = LinearSegmentedColormap.from_list
-    is_listed = colors is not None and len(colors) == N
     if is_listed:
         colormap = ListedColormap
 
